c6174881f885f28a58e3e8381f3ac53e41600c19
Dmytro Yantsybaiev <d.yantsybaiev@icloud.com>
2021-06-09 15:36:11 +0300

    Add the CoinData struct for catching JSON data. Add the performRequest method which makes an API request. Add the parseJSON method for parsing incoming data.

---
+++ ByteCoin/Model/CoinData.swift
@@ -0,0 +1,13 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Dmytro Yantsybaiev on 09.06.2021.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData: Codable {
+    let rate: Double
+}
--- ByteCoin/Model/CoinManager.swift
+++ ByteCoin/Model/CoinManager.swift
@@ -11,12 +11,39 @@ import Foundation
 struct CoinManager {
     
     let baseURL = "https://rest.coinapi.io/v1/exchangerate/BTC"
-    let apiKey = "YOUR_API_KEY_HERE"
+    let apiKey = "43770538-AA77-42F0-AE84-EED232846EA3"
     
-    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","USD","ZAR"]
+    let currencyArray = ["AUD", "BRL","CAD","CNY","EUR","GBP","HKD","IDR","ILS","INR","JPY","MXN","NOK","NZD","PLN","RON","RUB","SEK","SGD","UAH","USD","ZAR"]
 
     func getCoinPrice(for currency: String) {
-        
+        performRequest(with: "\(baseURL)/\(currency)?apikey=\(apiKey)")
     }
     
+    func performRequest(with urlString: String) {
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url) { (data, response, error) in
+                if error != nil {
+                    print("Error!")
+                }
+                if let safeData = data {
+                    if let bitcoinRate = parseJSON(safeData) {
+                        print(bitcoinRate)
+                    }
+                }
+            }
+            task.resume()
+        }
+    }
+    
+    func parseJSON(_ data: Data) -> Double? {
+        let decoder = JSONDecoder()
+        do {
+            let decodedData = try decoder.decode(CoinData.self, from: data)
+            return decodedData.rate
+        } catch {
+            print(error)
+            return nil
+        }
+    }
 }
